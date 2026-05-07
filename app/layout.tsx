e165668b7d8d47df924a3c043e4e42621598e9a2
JoaquinSangiorgio <sangiorgio.joaquin@uncuyo.edu.ar>
2026-05-07 17:51:35 -0300

    segundo deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Inter } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
-import { Toaster } from 'sonner' // 👈 1. Importamos el Toaster
+import { Toaster } from 'sonner'
 import './globals.css'
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
